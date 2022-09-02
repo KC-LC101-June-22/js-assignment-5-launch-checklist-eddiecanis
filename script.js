ace8dbbb4b714e9ae0b1289efa14675143a56718
eddiecanis <eddiecanis@gmail.com>
2022-09-01 21:17:31 -0500

    code for toby

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 // Write your JavaScript code here!
 
 window.addEventListener("load", function() {
+    let pilotInput = document.querySelector("input[name=pilotName]");
+        let pilot = pilotInput.value;
+ 
+        let copilotInput = document.querySelector("input[name=copilotName]");
+        let copilot = copilotInput.value;
+ 
+        let fuelInput = document.querySelector("input[name=fuelLevel]");
+        let fuelLevel = Number(fuelInput.value);
+ 
+        let cargoInput = document.querySelector("input[name=cargoMass]");
+        let cargoLevel = Number(cargoInput.value);
+
 
    let listedPlanets;
-   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
+   // Set listedPlanetsResponse equal to the value returned by calling myFetch() //
+   window.addEventListener("load", function(myFetch)
    let listedPlanetsResponse;
    listedPlanetsResponse.then(function (result) {
        listedPlanets = result;
@@ -11,6 +24,9 @@ window.addEventListener("load", function() {
    }).then(function () {
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
+ function result();
+  
+    }) 
+   
    
 });
--- scriptHelper.js
+++ scriptHelper.js
@@ -2,33 +2,58 @@
 require('isomorphic-fetch');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
-   // Here is the HTML formatting for our mission target div.
-   /*
-                <h2>Mission Destination</h2>
-                <ol>
-                    <li>Name: </li>
-                    <li>Diameter: </li>
-                    <li>Star: ${star}</li>
-                    <li>Distance from Earth: </li>
-                    <li>Number of Moons: </li>
-                </ol>
-                <img src="">
-   */
+    // Here is the HTML formatting for our mission target div. follow templet below
+
+    `<h2>Mission Destination</h2>
+        <ol>
+         <li>Name: ${name}</li>
+         <li>Diameter: ${diameter}</li>
+         <li>Star: ${star}</li>
+         <li>Distance from Earth: ${distance} </li>
+        <li>Number of Moons: ${moons} </li>
+        </ol>
+        <img src=imageUrl>
+`
 }
 
 function validateInput(testInput) {
-   
+    if (testInput === "") return "Empty"
+    if (isNaN(testInput)) return "Not a Number"
+    if (!isNaN(testInput)) return "Is a Number";
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
+    //use validate input to complete this fuction build test in form summistion, function in a function, checking and alerts
+    //function(function ())
+    //add all numbfield
+    if validateInput(pilot) === "Empty" || validateInput(copilot) === "Empty" || validateInput(fuelLevel) === "Empty" || validateInput(cargoLevel) === "Empty" {
+        alert("Please make sure all fields are filled out");
+    }
+    if validateInput(pilot) === "Is a Number" || validateInput(copilot) === "Is a Number" {
+        alert("Please make sure field is a name");
+    }
+    if validateInput(fuelLevel) === "Not a Number" || validateInput(cargoLevel) === "Not a Number"{
+        alert("Please make sure data is a number")
+    }
+    //1 button.addEventListener("click", function() 
+    //2 preventDefault()
+    //3 fuel and cargo mass should be a number and it enter as a text
+    //
+    return
 }
 
 async function myFetch() {
     let planetsReturned;
 
-    planetsReturned = await fetch().then( function(response) {
-        });
+    planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then(function (response) {
+        if (response.status >= 400) {
+            throw new Error("Bad response");
+        }
+        else {
+            return response.json();
+        }
+    });
+
 
     return planetsReturned;
 }
@@ -39,5 +64,5 @@ function pickPlanet(planets) {
 module.exports.addDestinationInfo = addDestinationInfo;
 module.exports.validateInput = validateInput;
 module.exports.formSubmission = formSubmission;
-module.exports.pickPlanet = pickPlanet; 
+module.exports.pickPlanet = pickPlanet;
 module.exports.myFetch = myFetch;
